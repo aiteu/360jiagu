1a75951505089e61551be94123a7da4580a3961d
David <david@dvid>
2019-07-21 22:56:17 +0800

    add 360 jiagu of python version 1.0

---
+++ run_jiagu.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# _*_ coding: UTF-8 _*_
+#360 jiagu script
+
+import os;
+
+def exec_360_jiagu():
+    os.chdir('/home/david/360gu/jiagu')
+    os.system('java -jar jiagu.jar -login 18149308903 love1005')
+    jiagu_cmd = 'java -jar jiagu.jar -jiagu /home/david/360gu/bbbao_533.apk /home/david/360gu/apk_jiagu -autosign'
+    result = os.system(jiagu_cmd)
+    if not result:
+        print "====" + str(result) + "  success"
+    else:
+        print "----failed"
+    return
+
+
+exec_360_jiagu();
