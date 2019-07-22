597e449e872536bcb8f8fcfe4bb97efedcefca8b
aiteu <aitality@163.com>
2019-07-22 11:24:29 +0800

    Update run_jiagu.py
    增加注释

--- run_jiagu.py
+++ run_jiagu.py
@@ -6,7 +6,7 @@ import os;
 
 def exec_360_jiagu():
     os.chdir('/home/david/360gu/jiagu')
-    os.system('java -jar jiagu.jar -login 18149308903 love1005')
+    os.system('java -jar jiagu.jar -login account pass')
     jiagu_cmd = 'java -jar jiagu.jar -jiagu /home/david/360gu/bbbao_533.apk /home/david/360gu/apk_jiagu -autosign'
     result = os.system(jiagu_cmd)
     if not result:
